87aa0acf5c9267ffcd1044f0ee582ee9f3dc86d5
DarkStarKhazix <wincentbastian3@gmail.com>
2020-06-04 20:32:38 +0800

    first-commit

---
+++ app/src/main/java/com/example/topik_corona/Auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.topik_corona.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.topik_corona.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
