7f45dc5cf7531005ef4ca7861450a8028331e6ec
DarkStarKhazix <wincentbastian3@gmail.com>
2020-06-16 17:50:40 +0800

    splashscreen

---
+++ app/src/main/java/com/example/topik_corona/Activity/FeatureActivity.kt
@@ -0,0 +1,12 @@
+package com.example.topik_corona.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeatureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature)
+    }
+}
